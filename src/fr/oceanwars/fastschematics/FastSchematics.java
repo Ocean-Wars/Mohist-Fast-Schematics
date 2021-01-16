5667c602db7ffb70425293e8b2704d9cc54b1eb2
Nicolas Went <nicolas.went@epita.fr>
2021-01-16 15:06:31 +0100

    [Command] WIP on command creation

--- src/fr/oceanwars/fastschematics/FastSchematics.java
+++ src/fr/oceanwars/fastschematics/FastSchematics.java
@@ -1,12 +1,13 @@
 package fr.oceanwars.fastschematics;
 
+import fr.oceanwars.fastschematics.commands.MCopy;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FastSchematics extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        this.getCommand("mcopy").setExecutor(new MCopy());
     }
 
     @Override
