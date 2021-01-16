d47979cbda7cc66c277b000a0f98e637384b374f
Nicolas Went <nicolas.went@epita.fr>
2021-01-16 14:46:49 +0100

    [Archi] First commit

---
+++ src/fr/oceanwars/fastschematics/FastSchematics.java
@@ -0,0 +1,16 @@
+package fr.oceanwars.fastschematics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FastSchematics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
---
+++ src/fr/oceanwars/fastschematics/commands/MCopy.java
@@ -0,0 +1,13 @@
+package fr.oceanwars.fastschematics.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class MCopy implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        return false;
+    }
+}
