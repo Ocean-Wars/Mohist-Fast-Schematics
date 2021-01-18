7acbf76bfc62e7fa7ee5b09271dc24585979d5fd
Nicolas Went <nicolas.went@epita.fr>
2021-01-18 18:55:57 +0100

    [Copy][Compression] Added compression for copy and paste

--- src/fr/oceanwars/fastschematics/utils/FileUtils.java
+++ src/fr/oceanwars/fastschematics/utils/FileUtils.java
@@ -4,12 +4,14 @@ import java.io.*;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.InflaterInputStream;
 
 public class FileUtils {
     public static final void writeObjectToFile(File file, Serializable obj) {
         try {
             FileOutputStream fileOut = new FileOutputStream(file);
-            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+            ObjectOutputStream objectOut = new ObjectOutputStream(new DeflaterOutputStream(fileOut));
             objectOut.writeObject(obj);
             objectOut.close();
         } catch (FileNotFoundException e) {
@@ -22,7 +24,7 @@ public class FileUtils {
     public static final Object readObjectFromFile(File file) {
         try {
             FileInputStream fileIn = new FileInputStream(file);
-            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+            ObjectInputStream objectIn = new ObjectInputStream(new InflaterInputStream(fileIn));
             Object obj = objectIn.readObject();
             objectIn.close();
             return obj;
